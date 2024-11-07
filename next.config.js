d1fde006897ed5aba4ec9509e57eac97557f8a5a
Riza Bachtiar <riza.bachtiar.rb@gmail.com>
2024-11-07 07:45:23 +0700

    feat: add security header

--- next.config.js
+++ next.config.js
@@ -1,46 +1,67 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "standalone",
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    async headers() {
-        return [
+  output: "standalone",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
           {
-            source: '/:path*',
-            headers: [
-              {
-                key: 'X-Robots-Tag',
-                value: 'index, follow',
-              },
-            ],
+            key: "X-Robots-Tag",
+            value: "index, follow",
           },
-        ];
-      },
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "cdn-h2h.s3.ap-southeast-1.amazonaws.com",
-                port: "",
-            },
-            {
-                protocol: "https",
-                hostname: "cdn.vcgamers.com",
-                port: "",
-            },
-            {
-                protocol: "https",
-                hostname: "cdn-panel.vcg.my.id",
-                port: "",
-            },
-            {
-                protocol: "https",
-                hostname: "s3-alpha-sig.figma.com",
-                port: "",
-            },
         ],
-    },
+      },
+      {
+        // Sets security headers for all routes
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+            //You can use SAMEORIGIN as a value also.
+          },
+        ],
+      },
+      {
+        // Sets security headers for all routes
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+        ],
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn-h2h.s3.ap-southeast-1.amazonaws.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.vcgamers.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn-panel.vcg.my.id",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "s3-alpha-sig.figma.com",
+        port: "",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
