f7bd612f8d7870072fbf50fd1e6f0e3843826aeb
Riza Bachtiar <riza.bachtiar.rb@gmail.com>
2025-08-12 19:16:32 +0700

    fix: fix gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import RootTemplateLayout from "./root-layout";
 import Loading from "./loading";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 export default function RootLayout({
   children,
@@ -18,33 +19,13 @@ export default function RootLayout({
             }`,
           }}
         />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-P25S2QGV');
-            `,
-          }}
-        />
       </head>
       <body className="min-h-screen bg-repeat antialiased bg-zinc-50">
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-P25S2QGV"
-            height="0"
-            width="0"
-            style="display:none;visibility:hidden"
-          ></iframe>`,
-          }}
-        />
         <Suspense fallback={<Loading />}>
           <RootTemplateLayout>{children}</RootTemplateLayout>
         </Suspense>
       </body>
+      <GoogleTagManager gtmId="GTM-P25S2QGV" />
     </html>
   );
 }
